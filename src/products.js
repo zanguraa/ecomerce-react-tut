5ae0b4db715c752804472102dc10614f1307c56d
Valeri Zanguri <zanguraa@gmail.com>
2023-09-17 01:11:42 +0400

    feat: added cart item logic with createContext

---
+++ src/products.js
@@ -0,0 +1,67 @@
+import product1 from "./assets/Apple_iPhone_12.png";
+import product2 from "./assets/Apple_iPhone_12_Pro.png";
+import product3 from "./assets/Apple_iPhone_12_Pro_Max.png";
+import product4 from "./assets/Apple_iPhone_12_Mini.png";
+import product5 from "./assets/Apple_iPhone_11.png";
+import product6 from "./assets/Apple_iPhone_11_Pro.png";
+import product7 from "./assets/Apple_iPhone_11_Pro.png";
+import product8 from "./assets/Apple_iPhone_SE.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    name: "Apple iPhone 12",
+    price: 799,
+    description: "A large phone with one of the best screens",
+    productImage: product1,
+  },
+  {
+    id: 2,
+    name: "Apple iPhone 12 Pro",
+    price: 999,
+    description: "A great phone with one of the best cameras",
+    productImage: product2,
+  },
+  {
+    id: 3,
+    name: "Apple iPhone 12 Pro Max",
+    price: 1099,
+    description: "A great phone with one of the best cameras",
+    productImage: product3,
+  },
+  {
+    id: 4,
+    name: "Apple iPhone 12 Mini",
+    price: 699,
+    description: "A great phone with one of the best cameras",
+    productImage: product4,
+  },
+  {
+    id: 5,
+    name: "Apple iPhone 11",
+    price: 599,
+    description: "A great phone with one of the best cameras",
+    productImage: product5,
+  },
+  {
+    id: 6,
+    name: "Apple iPhone 11 Pro",
+    price: 699,
+    description: "A great phone with one of the best cameras",
+    productImage: product6,
+  },
+  {
+    id: 7,
+    name: "Apple iPhone 11 Pro Max",
+    price: 799,
+    description: "A great phone with one of the best cameras",
+    productImage: product7,
+  },
+  {
+    id: 8,
+    name: "Apple iPhone SE",
+    price: 399,
+    description: "A great phone with one of the best cameras",
+    productImage: product8,
+  },
+];
